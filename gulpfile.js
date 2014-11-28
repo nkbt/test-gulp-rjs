aee0b0e847d31d9b478a8875d1a4fa097fb651e9
Nik Butenko <nik@butenko.me>
2014-11-28 17:22:30 +1100

    Dynamic building of multiple workers

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,27 @@ var amdOptimize = require("amd-optimize");
 var concat = require('gulp-concat');
 
 
-gulp.task("workers:first", function () {
 
-  return gulp.src("src/**/*.js")
-    // Traces all modules and outputs them in the correct order.
-    .pipe(amdOptimize("workers/first"))
-    .pipe(concat("first.js"))
+var through = require('through2');
+var path = require('path');
+
+var optimizeWorker = through.obj(function (file, enc, cb) {
+
+  var ext = path.extname(file.path),
+    name = path.basename(file.path, ext);
+
+  var stream = gulp.src("src/**/*.js")
+    .pipe(amdOptimize("workers/" + name))
+    .pipe(concat(name + ext))
     .pipe(gulp.dest("build/workers"));
+
+  return cb(null, stream);
 
 });
 
-gulp.task("workers:second", function () {
+gulp.task('default', function () {
 
-  return gulp.src("src/**/*.js")
-    // Traces all modules and outputs them in the correct order.
-    .pipe(amdOptimize("workers/second"))
-    .pipe(concat("second.js"))
-    .pipe(gulp.dest("build/workers"));
+  return gulp.src("src/workers/*.js")
+    .pipe(optimizeWorker)
 
 });
-
-gulp.task("default", ['workers:first', 'workers:second']);
